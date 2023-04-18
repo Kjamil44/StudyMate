83d76788aea14f09e60a0ef68a1bf3a3c6cb2669
Трајковски Мартин <martintrajkovski70@gmail.com>
2023-04-18 21:27:59 +0200

    [SMT5-14] Create login page - Created login page design

--- study-mate-ui/src/App.tsx
+++ study-mate-ui/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import './App.css';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './pages/RootLayout';
 import HomePage from './pages/HomePage';
-import { RouterProvider } from 'react-router-dom'
+import Login from './pages/login/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      {index: true, element: <HomePage />}
-    ]
-  }
+      { index: true, element: <HomePage /> },
+      { index: true, path: '/login', element: <Login /> },
+    ],
+  },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
