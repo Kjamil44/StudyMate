f980447adff2230f8859698bced901cdd83de171
Kjamil Kakaleski <kakaleskikjamil@gmail.com>
2023-04-21 19:13:17 +0200

    [SMT5-11] Make the Subject controller

---
+++ StudyMate.API/Handlers/SubjectHandlers/GetSubjects.cs
@@ -0,0 +1,55 @@
+﻿using Marten;
+using MediatR;
+using StudyMate.API.Enums;
+using StudyMate.API.Models;
+
+namespace StudyMate.API.Handlers.SubjectHandlers
+{
+    public class GetSubjects
+    {
+        public class Request : IRequest<Response>
+        {
+        }
+        public class Response
+        {
+            public class Item
+            {
+                public Guid Id { get; set; }
+                public Guid UserId { get; set; }
+                public string Name { get; set; }
+                public string Description { get; set; }
+                public int Grade { get; set; }
+                public DateTime StartDate { get; set; }
+                public DateTime EndDate { get; set; }
+                public Status Status { get; set; }
+            }
+            public IEnumerable<Item> Items { get; set; } = Enumerable.Empty<Item>();
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IDocumentSession _session;
+            public Handler(IDocumentSession session) => _session = session;
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var subjects = await _session.Query<Subject>().ToListAsync();
+                if (!subjects.Any())
+                    throw new Exception("No Subjects available");
+
+                return new Response
+                {
+                    Items = subjects.Select(subject => new Response.Item
+                    {
+                        Id = subject.Id,
+                        UserId = subject.UserId,
+                        Name = subject.Name,
+                        Description = subject.Description,
+                        Grade = subject.Grade,
+                        StartDate = subject.StartDate,
+                        EndDate = subject.EndDate,
+                        Status = subject.Status
+                    })
+                };
+            }
+        }
+    }
+}
---
+++ StudyMate.API/Handlers/SubjectHandlers/UpdateSubject.cs
@@ -0,0 +1,41 @@
+﻿using Marten;
+using MediatR;
+using StudyMate.API.Enums;
+using StudyMate.API.Models;
+
+namespace StudyMate.API.Handlers.SubjectHandlers
+{
+    public class UpdateSubject
+    {
+        public class Request : IRequest<Response>
+        {
+            public Guid Id { get; set; }
+            public string? Name { get; set; }
+            public string? Description { get; set; }
+            public int? Grade { get; set; }
+            public DateTime? StartDate { get; set; }
+            public DateTime? EndDate { get; set; }
+            public Status? Status { get; set; }
+        }
+        public class Response
+        {
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IDocumentSession _session;
+            public Handler(IDocumentSession session) => _session = session;
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var subject = await _session.LoadAsync<Subject>(request.Id);
+                if (subject == null)
+                    throw new Exception("Subject not found");
+
+                subject.UpdateSubject(request.Name, request.Description, request.Grade, request.StartDate, request.EndDate, request.Status);
+                _session.Store(subject);
+                await _session.SaveChangesAsync();
+
+                return new Response();
+            }
+        }
+    }
+}
