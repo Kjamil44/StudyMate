b542e8bff8b193bff4051ef474fc2706855725b3
Marko Andonov <marko.20112000@gmail.com>
2023-04-22 12:59:52 +0200

    make the subjects index page

--- study-mate-ui/src/App.tsx
+++ study-mate-ui/src/App.tsx
@@ -4,6 +4,30 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './pages/RootLayout';
 import HomePage from './pages/HomePage';
 import Login from './pages/login/Login';
+import SubjectIndexPage from './pages/subject/Index';
+
+const subjects = [
+  {
+    subjectId: 1,
+    title: 'Mathematics',
+    description: 'The study of numbers, quantities, and shapes.'
+  },
+  {
+    subjectId: 2,
+    title: 'Science',
+    description: 'The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology.'
+  },
+  {
+    subjectId: 3,
+    title: 'History',
+    description: 'The study of past events and their significance.'
+  },
+  {
+    subjectId: 4,
+    title: 'English',
+    description: 'The study of language, literature, and writing.'
+  }
+];
 
 const router = createBrowserRouter([
   {
@@ -12,6 +36,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { index: true, path: '/login', element: <Login /> },
+      { index: true, path: '/subjects', element: <SubjectIndexPage subjects={subjects} /> },
     ],
   },
 ]);
---
+++ study-mate-ui/src/pages/subject/Index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import SubjectComponent from '../../components/subject/SubjectComponent';
+
+interface Props {
+  subjects: Array<Subject>;
+}
+
+interface Subject {
+  subjectId: number;
+  title: string;
+  description: string;
+}
+
+const SubjectIndexPage: React.FC<Props>  = ({ subjects }) => {
+  const borderDashed = {
+    border: "0.5px dashed #4e4e4e",
+  }
+  return (
+    <div className="container">
+    <div className="row">
+    {subjects.map((subject, i) => (
+      <SubjectComponent key={i} subject={subject} />
+    ))}
+    <div className="col-12 col-md-4 col-lg-4 p-3 cursor-pointer">
+      <Link
+          className="text-decoration-none d-flex flex-column justify-content-center align-items-center rounded rounded-3 h-100 w-100 p-5"
+          style={borderDashed} to={'add-subject'}      >
+        <div
+          className="mb-1 bg-success text-dark rounded rounded-circle px-2 d-flex align-items-center justify-content-center"
+        >
+          +
+        </div>
+        <div className="fs-14px fw-bold text-dark">
+          Add subject
+        </div>
+      </Link>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default SubjectIndexPage
