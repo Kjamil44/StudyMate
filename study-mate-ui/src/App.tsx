0318f05e5cde011881893ec311c7786a8b6be7cc
Трајковски Мартин <martintrajkovski70@gmail.com>
2023-04-23 10:48:39 +0200

    [SMT5-19] Create dockerfiles for frontend and backend - Created dockerfile for frontend

--- study-mate-ui/src/App.tsx
+++ study-mate-ui/src/App.tsx
@@ -1,36 +1,38 @@
 import React from 'react';
-import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RootLayout from './pages/RootLayout';
+import './App.css';
+import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 import Login from './pages/login/Login';
+import RegisterPage from './pages/RegisterPage';
+import RootLayout from './pages/RootLayout';
 import SubjectIndexPage from './pages/subject/Index';
 
 const subjects = [
   {
     subjectId: 1,
     title: 'Mathematics',
-    description: 'The study of numbers, quantities, and shapes.'
+    description: 'The study of numbers, quantities, and shapes.',
   },
   {
     subjectId: 2,
     title: 'Science',
-    description: 'The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology.'
+    description:
+      'The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology. The study of the natural world, including physics, chemistry, and biology.',
   },
   {
     subjectId: 3,
     title: 'History',
-    description: 'The study of past events and their significance.'
+    description: 'The study of past events and their significance.',
   },
   {
     subjectId: 4,
     title: 'English',
-    description: 'The study of language, literature, and writing.'
-  }
+    description: 'The study of language, literature, and writing.',
+  },
 ];
 
-import ErrorPage from './pages/ErrorPage';
-import RegisterPage from './pages/RegisterPage';
+
 
 const router = createBrowserRouter([
   {
